d7fc063b94051acbb7b6bd1503bde8af392e30e0
Obalin <balin.orhan@gmail.com>
2022-05-21 10:34:06 +0300

    Homework - done !

--- src/main/java/ru/gb/obalin/Homework7/Cat.java
+++ src/main/java/ru/gb/obalin/Homework7/Cat.java
@@ -1,15 +1,24 @@
 package ru.gb.obalin.Homework7;
 
 public class Cat {
-    protected String name;
-    protected int appetite;
-    public boolean fullness;
+    private String name;
+    private int appetite;
+    private boolean fullness;
     public Cat(String name, int appetite, boolean fullness) {
         this.name = name;
         this.appetite = appetite;
         this.fullness = fullness;
     }
-    public void eat(Plate p) {
-        p.decreaseFood(appetite);
+    public void setFull(boolean fullness) {
+        this.fullness = fullness;
+    }
+    public boolean  eat(Plate p) {
+        return p.decreaseFood(appetite);
+    }
+    public String toString() {
+        return name + ", Аппетит= " + appetite + ", Покушал? " + fullness;
+    }
+    public void info() {
+        System.out.println(this);
     }
 }
