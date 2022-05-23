b90c0de780036bb0511b6d97d9ba653effadfba0
Obalin <balin.orhan@gmail.com>
2022-05-23 12:56:15 +0300

    Homework - done !

---
+++ src/main/java/ru/gb/obalin/Homework8/Homework8.java
@@ -0,0 +1,16 @@
+package ru.gb.obalin.Homework8;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Homework8 {
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                CalculatorFrame frame = new CalculatorFrame();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
