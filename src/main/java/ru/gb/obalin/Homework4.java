d9d5cc4df5d61c7024731cef0ad6252f7b0249c0
Obalin <balin.orhan@gmail.com>
2022-06-09 16:21:23 +0300

    Homework - done !

---
+++ src/main/java/ru/gb/obalin/Homework4.java
@@ -0,0 +1,10 @@
+package ru.gb.obalin;
+
+import javax.swing.*;
+
+public class Homework4 {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Frame::new);
+    }
+}
+
